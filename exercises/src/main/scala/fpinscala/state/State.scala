f730278e316e8eafd84829ba05126f4f49814ab6
MMMarcy <steiner.marcello@gmail.com>
2014-11-13 22:01:14 +0100

    Started chapter 6 and reached exercise 6.7. TODO: going through the stacktrace of 6.7 again.

--- exercises/src/main/scala/fpinscala/state/State.scala
+++ exercises/src/main/scala/fpinscala/state/State.scala
@@ -1,6 +1,5 @@
 package fpinscala.state
 
-
 trait RNG {
   def nextInt: (Int, RNG) // Should generate a random `Int`. We'll later define other functions in terms of `nextInt`.
 }
@@ -24,35 +23,85 @@ object RNG {
   def unit[A](a: A): Rand[A] =
     rng => (a, rng)
 
-  def map[A,B](s: Rand[A])(f: A => B): Rand[B] =
+  def map[A, B](s: Rand[A])(f: A => B): Rand[B] =
     rng => {
       val (a, rng2) = s(rng)
       (f(a), rng2)
     }
 
-  def nonNegativeInt(rng: RNG): (Int, RNG) = ???
+  //Exercie 6.1
+  def nonNegativeInt(rng: RNG): (Int, RNG) = {
+    val (x, s1) = rng.nextInt;
+    if (x == Int.MinValue)
+      nonNegativeInt(s1)
+    else
+      (Math.abs(x), s1)
+  }
 
-  def double(rng: RNG): (Double, RNG) = ???
+  //Exrcise 6.2
+  def double(rng: RNG): (Double, RNG) = {
+    val (x, s1) = nonNegativeInt(rng)
+    x.toDouble / Int.MaxValue -> s1
+  }
 
-  def intDouble(rng: RNG): ((Int,Double), RNG) = ???
+  //Exercise 6.3
+  def intDouble(rng: RNG): ((Int, Double), RNG) = {
+    val (x1, s1) = double(rng)
+    val (x2, s2) = s1.nextInt
+    (x2 -> x1) -> s2
+  }
 
-  def doubleInt(rng: RNG): ((Double,Int), RNG) = ???
+  def doubleInt(rng: RNG): ((Double, Int), RNG) = {
+    val ((i, d), s1) = intDouble(rng)
+    (d, i) -> s1
+  }
 
-  def double3(rng: RNG): ((Double,Double,Double), RNG) = ???
+  def double3(rng: RNG): ((Double, Double, Double), RNG) = {
+    val (d1, s1) = double(rng)
+    val (d2, s2) = double(s1)
+    val (d3, s3) = double(s2)
+    (d1, d2, d3) -> s3
+  }
 
-  def ints(count: Int)(rng: RNG): (List[Int], RNG) = ???
+  def ints(count: Int)(rng: RNG): (List[Int], RNG) = {
+    @annotation.tailrec
+    def go(count: Int)(rng: RNG)(acc: List[Int]): (List[Int], RNG) = {
+      if (count == 0)
+        acc -> rng
+      else {
+        val (x, s1) = rng.nextInt
+        go(count - 1)(s1)(x :: acc)
+      }
+    }
+    go(count)(rng)(Nil)
+  }
 
-  def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = ???
+  //Exercise 6.5
+  def doubleViaMap: Rand[Double] =
+    map(nonNegativeInt)(i => i.toDouble / Int.MaxValue)
 
-  def sequence[A](fs: List[Rand[A]]): Rand[List[A]] = ???
+  //Exercise 6.6
+  def map2[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = rng => {
+    val (xa, s1) = ra(rng)
+    val (xb, s2) = rb(s1)
+    (f(xa, xb), s2)
+  }
 
-  def flatMap[A,B](f: Rand[A])(g: A => Rand[B]): Rand[B] = ???
+  def both[A, B](ra: Rand[A], rb: Rand[B]): Rand[(A, B)] =
+    map2(ra, rb)(_ -> _)
+
+  //Exercise 6.7
+  def sequence[A](fs: List[Rand[A]]): Rand[List[A]] = {
+    fs.foldRight(unit(List[A]()))((f,acc) => map2(f,acc)(_ :: _))
+  }
+
+  def flatMap[A, B](f: Rand[A])(g: A => Rand[B]): Rand[B] = ???
 }
 
-case class State[S,+A](run: S => (A, S)) {
+case class State[S, +A](run: S => (A, S)) {
   def map[B](f: A => B): State[S, B] =
     sys.error("todo")
-  def map2[B,C](sb: State[S, B])(f: (A, B) => C): State[S, C] =
+  def map2[B, C](sb: State[S, B])(f: (A, B) => C): State[S, C] =
     sys.error("todo")
   def flatMap[B](f: A => State[S, B]): State[S, B] =
     sys.error("todo")
